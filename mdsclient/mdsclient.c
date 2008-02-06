b09f70f512c6ef898605d4adc1eb59230df2cfd1
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2008-02-06 11:33:35 +0000

    - mdsclient: finally it works! Needed to include only ipdesc.h and link against MdsIpShr.so!
    git-svn-id: file:///svn_repos/mexfunctions.new@10 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mdsclient/mdsclient.c
+++ mdsclient/mdsclient.c
@@ -15,13 +15,16 @@
 #include "mex.h"
 #include "matrix.h"
 #include "math.h"
-#include "mdslib.h"
+#include "ipdesc.h"
 
 #define min(a,b) ((a)<(b) ? (a) : (b))
 #define max(a,b) ((a)>(b) ? (a) : (b))
 
 #define status_ok(status) (((status) & 1) == 1)
 
+#ifndef INVALID_SOCKET
+#define INVALID_SOCKET -1
+#endif
 
 void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
 {
@@ -31,7 +34,7 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
 	char dtype=DTYPE_CSTRING;
 	int nbytes=0;
 
-  	int idesc = descr2(&dtype_long, &zero);
+/*	int idesc = descr2(&dtype_long, &zero);*/
     
 	register int i;
 	unsigned char nargs = 0;
@@ -51,14 +54,21 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
 	L[0] = mxCreateNumericArray(1,Dims,mxINT32_CLASS,mxREAL);
 	out = (int*) mxGetPr(L[0]);
     
-	sock = MdsConnect("plaspc03.ucsd.edu");
+	sock = ConnectToMds("plaspc03.ucsd.edu");
 	if (sock == INVALID_SOCKET) {
 		mxErrMsgTxt("Could not connect to server");
 	}
 
-	stat = MdsOpen("csdx",&shot);
+	stat = MdsOpen(sock,"csdx",shot);
+
+	struct descrip ans_arg;
+	
+	stat = MdsValue(sock, ptr, &ans_arg);
+
+	*out = *((int*)ans_arg.ptr);
 
 
+/*
 	struct descrip exparg, *arg, *ans_arg;
 	ans_arg = arg = MakeDescrip(&exparg,DTYPE_CSTRING,0,0,ptr);
 
@@ -76,22 +86,11 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
 	printf("dtype=%d, len=%d, ndims=%d, dims[0]=%d, numbytes=%d\n",ans_arg->dtype,ans_arg->length,ans_arg->ndims,ans_arg->dims[0],numbytes);
 	
 	*out = *((int*)dptr);
-
-/*
-	stat = SendArg(sock, 0, DTYPE_CSTRING, nargs, len, ndims, dims, ptr);
-	if (!status_ok(stat)) {
-		mxErrMsgTxt("SendArg error");
-	}
-
-	stat = GetAnswerInfo(sock, &dtype, &len, &ndims, dims, &nbytes, dptr);
-	if (!status_ok(stat)) {
-		mxErrMsgTxt("GetAnswerInfo error");
-	}
 */
 
 /*	stat = MdsValue2(ptr,&idesc,out,&zero);*/
 	
-	stat = MdsClose("csdx",&shot);
+	stat = MdsClose(sock);
 
 	stat = DisconnectFromMds(sock);
 
