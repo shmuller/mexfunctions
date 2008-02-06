36e87d270a041eb1a4297d21a066070d5c48d30d
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2008-02-06 13:12:41 +0000

    - mdsclient: make a local function myMdsValue() copying MdsValue() from mdsipshr.c. This works.
    git-svn-id: file:///svn_repos/mexfunctions.new@12 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mdsclient/mdsclient.c
+++ mdsclient/mdsclient.c
@@ -16,6 +16,8 @@
 #include "matrix.h"
 #include "math.h"
 #include "ipdesc.h"
+#include "stdarg.h"
+#include "string.h"
 
 #define min(a,b) ((a)<(b) ? (a) : (b))
 #define max(a,b) ((a)>(b) ? (a) : (b))
@@ -25,6 +27,10 @@
 #ifndef INVALID_SOCKET
 #define INVALID_SOCKET -1
 #endif
+
+
+int myMdsValue(SOCKET sock, char *expression, ...);
+
 
 void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
 {
@@ -51,15 +57,114 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
 
 	int idx=0, nargs=0;
 	struct descrip exparg, *arg=&exparg;
+    	int numbytes = 0;
+    	void *dptr;
+    	void *mem = 0;
 /*
-	arg = MakeDescrip((struct descrip *)&exparg,DTYPE_CSTRING,0,0,expression);
-	status = SendArg(sock, idx, arg->dtype, nargs, ArgLen(arg), arg->ndims, arg->dims, arg->ptr);
-*/	
-	stat = MdsValue(sock, expression, arg);
+	arg = MakeDescrip(&exparg,DTYPE_CSTRING,0,0,expression);
+	stat = SendArg(sock, idx, arg->dtype, nargs, ArgLen(arg), arg->ndims, arg->dims, arg->ptr);
+	
+	printf("dtype=%d, len=%d, ndims=%d, dims[0]=%d, numbytes=%d\n",arg->dtype,arg->length,arg->ndims,arg->dims[0],numbytes);
+
+	stat = GetAnswerInfoTS(sock, &arg->dtype, &len, &arg->ndims, arg->dims, &numbytes, &dptr, &mem);
+    	arg->length = len;
+
+	printf("dtype=%d, len=%d, ndims=%d, dims[0]=%d, numbytes=%d\n",arg->dtype,arg->length,arg->ndims,arg->dims[0],numbytes);
+
+	*((int*)out) = *((int*)dptr);
+*/
+	
+	stat = myMdsValue(sock, expression, arg, NULL);
 	*((int*)out) = *((int*)arg->ptr);
 
 	stat = MdsClose(sock);
 
 	stat = DisconnectFromMds(sock);
+	
+#ifdef _USE_VARARGS
+	printf("Hallo!\n");
+#endif
 
 }
+
+
+
+
+
+#ifdef _USE_VARARGS
+int myMdsValue(va_alist) va_dcl
+{
+  SOCKET sock;
+  char *expression;
+#else
+int myMdsValue(SOCKET sock, char *expression, ...)
+{
+#endif
+
+  va_list incrmtr;
+  int a_count;
+  int i;
+  unsigned char nargs;
+  unsigned char idx;
+  int status = 1;
+  struct descrip exparg;
+  struct descrip *ans_arg;
+  struct descrip *arg = &exparg;
+#ifdef _USE_VARARGS
+  va_start(incrmtr);
+  sock = va_arg(incrmtr, SOCKET);
+  expression = va_arg(incrmtr, char *);
+#else
+  va_start(incrmtr, expression);
+#endif
+  for (a_count = 1; arg != NULL; a_count++)
+  {
+    ans_arg=arg;
+    arg = va_arg(incrmtr, struct descrip *);
+  }
+#ifdef _USE_VARARGS
+  va_start(incrmtr);
+  sock = va_arg(incrmtr, SOCKET);
+  expression = va_arg(incrmtr, char *);
+#else
+  va_start(incrmtr, expression);
+#endif
+  nargs = a_count - 2;
+  arg = MakeDescrip((struct descrip *)&exparg,DTYPE_CSTRING,0,0,expression);
+  for (i=1;i<a_count-1 && (status & 1);i++)
+  {
+    idx = i - 1;
+    status = SendArg(sock, idx, arg->dtype, nargs, ArgLen(arg), arg->ndims, arg->dims, arg->ptr);
+    arg = va_arg(incrmtr, struct descrip *);
+  }
+  va_end(incrmtr);
+  if (status & 1)
+  {
+    short len;
+    int numbytes;
+    void *dptr;
+    void *mem = 0;
+    status = GetAnswerInfoTS(sock, &ans_arg->dtype, &len, &ans_arg->ndims, ans_arg->dims, &numbytes, &dptr, &mem);
+    ans_arg->length = len;
+    if (numbytes)
+    {
+      if (ans_arg->dtype == DTYPE_CSTRING)
+      {
+        ans_arg->ptr = malloc(numbytes+1);
+        ((char *)ans_arg->ptr)[numbytes] = 0;
+      }
+      else if (numbytes > 0)
+        ans_arg->ptr = malloc(numbytes);
+      if (numbytes > 0)
+        memcpy(ans_arg->ptr,dptr,numbytes);
+    }
+    else
+      ans_arg->ptr = NULL;
+    if (mem) free(mem);
+  }
+  else
+    ans_arg->ptr = NULL;
+  return status;
+}
+
+
