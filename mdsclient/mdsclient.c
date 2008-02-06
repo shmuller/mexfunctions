ef99903789bcc1e4b1f3f3a74749f7993985a4eb
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2008-02-06 12:10:09 +0000

    - mdsclient: move towards using low-level functions again to be able to write into   MATLAB array directly.
    git-svn-id: file:///svn_repos/mexfunctions.new@11 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mdsclient/mdsclient.c
+++ mdsclient/mdsclient.c
@@ -29,70 +29,37 @@
 void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
 {
 	SOCKET sock;
-	int stat,shot=0,zero=0;
-	int dtype_long = DTYPE_LONG;
-	char dtype=DTYPE_CSTRING;
-	int nbytes=0;
+	int stat,shot=0,zero=0;    
 
-/*	int idesc = descr2(&dtype_long, &zero);*/
-    
-	register int i;
-	unsigned char nargs = 0;
 	char ndims = mxGetNumberOfDimensions(R[0]);
-	const mwSize *Dims = mxGetDimensions(R[0]);
+	const mwSize *dims = mxGetDimensions(R[0]);
 	short len = mxGetNumberOfElements(R[0]);
-	char *ptr = malloc((len+1)*sizeof(char));
+	char *expression = malloc((len+1)*sizeof(char));
 
-	mxGetString(R[0],ptr,len+1);
-	printf("%s\n",ptr);    
+	mxGetString(R[0],expression,len+1);
+	printf("%s\n",expression);    
 
-	int *dims = malloc(ndims*sizeof(int));
-	for(i=0; i<ndims; i++) dims[i] = (int)Dims[i];
-
-	int *out;
-    
-	L[0] = mxCreateNumericArray(1,Dims,mxINT32_CLASS,mxREAL);
-	out = (int*) mxGetPr(L[0]);
+	L[0] = mxCreateNumericArray(1,dims,mxINT32_CLASS,mxREAL);
+	void *out = (void*) mxGetPr(L[0]);
     
 	sock = ConnectToMds("plaspc03.ucsd.edu");
 	if (sock == INVALID_SOCKET) {
-		mxErrMsgTxt("Could not connect to server");
+		mexErrMsgTxt("Could not connect to server");
 	}
 
 	stat = MdsOpen(sock,"csdx",shot);
 
-	struct descrip ans_arg;
-	
-	stat = MdsValue(sock, ptr, &ans_arg);
-
-	*out = *((int*)ans_arg.ptr);
-
-
+	int idx=0, nargs=0;
+	struct descrip exparg, *arg=&exparg;
 /*
-	struct descrip exparg, *arg, *ans_arg;
-	ans_arg = arg = MakeDescrip(&exparg,DTYPE_CSTRING,0,0,ptr);
+	arg = MakeDescrip((struct descrip *)&exparg,DTYPE_CSTRING,0,0,expression);
+	status = SendArg(sock, idx, arg->dtype, nargs, ArgLen(arg), arg->ndims, arg->dims, arg->ptr);
+*/	
+	stat = MdsValue(sock, expression, arg);
+	*((int*)out) = *((int*)arg->ptr);
 
-	printf("dtype=%d, len=%d, ndims=%d, dims[0]=%d, numbytes=%d\n",arg->dtype,ArgLen(arg),arg->ndims,arg->dims[0],0);
-	
-	int idx=0;
-    	stat = SendArg(sock, idx, arg->dtype, nargs, ArgLen(arg), arg->ndims, arg->dims, arg->ptr);
-
-    	int numbytes;
-    	void *dptr;
-    	void *mem = 0;
-    	stat = GetAnswerInfoTS(sock, &ans_arg->dtype, &len, &ans_arg->ndims, ans_arg->dims, &numbytes, &dptr, &mem);
-	ans_arg->length = len;
-
-	printf("dtype=%d, len=%d, ndims=%d, dims[0]=%d, numbytes=%d\n",ans_arg->dtype,ans_arg->length,ans_arg->ndims,ans_arg->dims[0],numbytes);
-	
-	*out = *((int*)dptr);
-*/
-
-/*	stat = MdsValue2(ptr,&idesc,out,&zero);*/
-	
 	stat = MdsClose(sock);
 
 	stat = DisconnectFromMds(sock);
-
 
 }
