0f2f578ac9a039a4fed5e3b719e08cdb4823ddfa
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2008-02-08 06:12:11 +0000

    - Add functions to download signals.
    git-svn-id: file:///svn_repos/mexfunctions.new@25 ad4b1c03-8ef8-444d-a533-6d794d5f2280

---
+++ mdsclient/MdsGetSig.m
@@ -0,0 +1,18 @@
+function [x,t] = MdsGetSig(sock,node)
+%MdsGetSig(sock,node)
+%   Get signal
+%
+%   S. H. Muller, 2008/02/07
+
+expr = ['getsigcal(',node,')'];
+f = mdsclient('mdsvalue',sock,expr);
+
+expr = ['raw_of(',node,')'];
+x = mdsclient('mdsvalue',sock,expr);
+
+x = f(1)*cast(x,class(f))+f(2);
+
+if nargout > 1
+    t = MdsGetTime(sock,node);
+    t = t(1:length(x));
+end
---
+++ mdsclient/MdsGetTime.m
@@ -0,0 +1,13 @@
+function t = MdsGetTime(sock,node)
+%MdsGetTime(sock,node)
+%   Get signal time base
+%
+%   S. H. Muller, 2008/02/07
+
+expr = ['_t=dim_of(',node,'); [slope_of(axis_of(_t)),_t[0]]'];
+t = 1000*mdsclient('mdsvalue',sock,expr);
+
+expr = '_i=dim_of(data(_t)); [_i[0],_i[size(_i)-1]]';
+i = mdsclient('mdsvalue',sock,expr);
+
+t = cast(i(1):i(2),class(t))*t(1)+t(2);
