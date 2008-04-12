a50409113c9653bb0be06b16222185bb8783c86d
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2008-04-12 07:30:07 +0000

    - Compile 32 and 64 bit versions on plaspc01 using Matlab 7.5.
    git-svn-id: file:///svn_repos/mexfunctions.new@40 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mdsclient/makefile.m
+++ mdsclient/makefile.m
@@ -10,6 +10,9 @@ switch computer
         mds = {'-IC:\PROGRA~1\MDSplus\DEVTOOLS\include','C:\PROGRA~1\MDSplus\DEVTOOLS\lib\MdsIpShr.lib'};
     case 'GLNX86'
         opt = {'COPTIMFLAGS=-O3','LDOPTIMFLAGS=-O3'};
+        mds = {'-I/usr/local/mdsplus/include','-L/usr/local/mdsplus/lib32','-lMdsIpShr'};
+    case 'GLNXA64'
+        opt = {'COPTIMFLAGS=-O3','LDOPTIMFLAGS=-O3'};
         mds = {'-I/usr/local/mdsplus/include','-L/usr/local/mdsplus/lib','-lMdsIpShr'};
 end
 
