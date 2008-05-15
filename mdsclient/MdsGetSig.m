a9b0017e0d7fb4eda2f982f5216b9547dc38326f
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2008-05-15 04:15:30 +0000

    - mdsclient/MdsGetSig.m: Using getsigcal.fun is still faster over the network, but its availability should   not be assumed by default. Solution: Provide several download modes, with default 'full'.
    git-svn-id: file:///svn_repos/mexfunctions.new@43 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mdsclient/MdsGetSig.m
+++ mdsclient/MdsGetSig.m
@@ -1,18 +1,30 @@
-function [x,t] = MdsGetSig(sock,node)
+function [x,t] = MdsGetSig(sock,node,mode)
 %MdsGetSig(sock,node)
 %   Get signal
 %
 %   S. H. Muller, 2008/02/07
 
-% expr = ['getsigcal(',node,')'];
-% f = mdsclient('mdsvalue',sock,expr);
-% 
-% expr = ['raw_of(',node,')'];
-% x = mdsclient('mdsvalue',sock,expr);
-% 
-% x = f(1)*cast(x,class(f))+f(2);
+if nargin < 3
+    mode = 'full';
+end
 
-x = mdsclient('mdsvalue',sock,node);
+switch mode
+    case 'full'
+        x = mdsclient('mdsvalue',sock,node);
+        
+    case 'raw'
+        expr = ['raw_of(',node,')'];
+        x = mdsclient('mdsvalue',sock,expr);
+        
+    case 'netw'
+        expr = ['getsigcal(',node,')'];
+        f = mdsclient('mdsvalue',sock,expr);
+
+        expr = ['raw_of(',node,')'];
+        x = mdsclient('mdsvalue',sock,expr);
+
+        x = f(1)*cast(x,class(f))+f(2);
+end
 
 if nargout > 1
     t = MdsGetTime(sock,node);
