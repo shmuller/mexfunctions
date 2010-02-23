6611301d1d26f76441e53a3272f3614fecd2b4f5
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2010-02-23 06:08:47 +0000

    - mediansmoothmex: Abandon previous approach with replacing one point in sorted list.   This seems more promising.
    git-svn-id: file:///svn_repos/mexfunctions.new@65 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mediansmoothmex/mediansmoothmex.c
+++ mediansmoothmex/mediansmoothmex.c
@@ -15,52 +15,96 @@
 #include "matrix.h"
 #include "math.h"
 
-#define torben median_double
-#define elem_type double
-#include "torben.c"
-#undef torben
-#undef elem_type
+typedef struct {
+    int ind;
+    const double *elem;
+} container;
 
-#define torben median_float
-#define elem_type float
-#include "torben.c"
-#undef torben
-#undef elem_type
+int compar(const void *a, const void *b)
+{
+    double d = *((const container*)b)->elem - *((const container*)a)->elem;
+    return (d<0)-(d>0);
+}
 
-#define torben median_int
-#define elem_type int
-#include "torben.c"
-#undef torben
-#undef elem_type
+void replace(container *C, const int M, const double *x, 
+    const int del, const int ind)
+{
+    register int i;
+    container *b, *e;
+    double xi = x[ind];
+    
+    /* find element to be replaced or position of new element */
+    for (i=0,b=C; i<M && b->ind != del && *b->elem < xi; i++,b++);
+    
+    if (b->ind == del) {
+        /* found element to be replaced - find new element now */
+        for (e=b+1,i++; i<M && *e->elem < xi; i++,e++); e--;
+        if (e > b) memmove(b,b+1,(e-b)*sizeof(container));
+        e->ind = ind;
+        e->elem = x+ind;
+    } else {
+        /* found position of new element - find old element now */
+        for (e=b; i<M && e->ind != del; i++,e++);
+        if (e > b) memmove(b+1,b,(e-b)*sizeof(container));
+        b->ind = ind;
+        b->elem = x+ind;
+    }
+}
 
-typedef int (*medianfcn) (const void*, const int);
+void print(const container *C, const int M)
+{
+    register int i;
+    const container *c;
+    
+    for (i=0, c=C; i<M; i++,c++) {
+        printf("%i: %f\n", c->ind, *c->elem);
+    }
+    printf("\n");
+}
 
 void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
 {
     register int i;
+    size_t M;
     
     const mwSize ndims = mxGetNumberOfDimensions(R[0]);
     const mwSize *dims = mxGetDimensions(R[0]);
     const mxClassID mxID = mxGetClassID(R[0]);
     const size_t N = mxGetNumberOfElements(R[0]);
     const size_t bytes = mxGetElementSize(R[0]);
-    const void *x = mxGetData(R[0]);
+    const double *x = mxGetData(R[0]);
     
     const int *w = mxGetData(R[1]);
-    
-    medianfcn median;
-    
-    switch (mxID) {
-        case mxDOUBLE_CLASS: median = median_double; break;
-        case mxSINGLE_CLASS: median = median_float;  break;
-        case mxINT32_CLASS:  median = median_int;    break;
-        default:
-            mexErrMsgTxt("Unsupported data type");
-    }
     
     L[0] = mxCreateNumericArray(ndims,dims,mxINT32_CLASS,mxREAL);
     int *ind = mxGetData(L[0]);
     
+    M = 2*(*w)+1;
+    container *C, *c;
+    C = malloc(M*sizeof(container));
+    
+    for (i=0, c=C; i<M; i++,c++) {
+        c->ind = i;
+        c->elem = x+i;
+    }
+    
+    ind += *w+1;
+    
+    qsort(C,M,sizeof(container),compar);
+    ind[-1] = C[M/2].ind;
+    
+    /* print(C,M); */
+    
+    for (i=0; i<N-M; i++) {
+        replace(C,M,x,i,i+M);
+        ind[i] = C[*w].ind;
+        
+        /* print(C,M); */
+    }
+    
+    free(C);
+    
+    /*
     if (*w+1 >= N) {
         int t = median(x,N);
         for (i=0; i<N; i++) *ind++ = t;
@@ -75,5 +119,5 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
             *ind++ = i + median(x+i*bytes, N-i);
         }
     }
-    
+    */
 }
