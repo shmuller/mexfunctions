5080c5c67389e51c387a6b6a41dfad2b73443ce6
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2010-03-25 19:54:58 +0000

    - mediansmoothmex/mediansmooth.m: Correct for boundary effects by   by default by using the mirroring technique from filtfilt.
    git-svn-id: file:///svn_repos/mexfunctions.new@79 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mediansmoothmex/mediansmooth.m
+++ mediansmoothmex/mediansmooth.m
@@ -1,13 +1,28 @@
-function y = mediansmooth(x,w)
-%y = mediansmooth(x,w)
+function y = mediansmooth(x,w,corr_bdry)
+%y = mediansmooth(x,w,corr_bdry)
 %   Sliding median filter of width 2*w+1.
 %
 %   S. H. Muller, 2010/02/23
 
+if nargin < 3
+    corr_bdry = 1;
+end
+
 [N,J] = size(x);
 y = zeros(size(x),class(x));
 
-for j = 1:J
-    ind = mediansmoothmex(x(:,j),int32(w));
-    y(:,j) = x(ind+1,j);
+if corr_bdry
+    for j = 1:J
+        x0 = 2.*x(1,j)-x(w+1:-1:2,j);
+        x1 = 2.*x(N,j)-x(N-1:-1:N-w,j);
+        xj = [x0; x(:,j); x1];
+
+        ind = mediansmoothmex(xj,int32(w));
+        y(:,j) = xj(ind(w+1:w+N)+1);
+    end
+else
+    for j = 1:J
+        ind = mediansmoothmex(x(:,j),int32(w));
+        y(:,j) = x(ind+1,j);
+    end
 end
