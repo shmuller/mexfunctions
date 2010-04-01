6d692ff138bd01192808339660361313f8c25001
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2010-04-01 23:11:04 +0000

    - mediansmoothmex/mediansmooth.m: Use sm_extrap.m to generate extrapolations to reduce   boundary effects.
    git-svn-id: file:///svn_repos/mexfunctions.new@80 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mediansmoothmex/mediansmooth.m
+++ mediansmoothmex/mediansmooth.m
@@ -13,8 +13,7 @@ y = zeros(size(x),class(x));
 
 if corr_bdry
     for j = 1:J
-        x0 = 2.*x(1,j)-x(w+1:-1:2,j);
-        x1 = 2.*x(N,j)-x(N-1:-1:N-w,j);
+        [x0,x1] = sm_extrap(x(:,j),w,'polyfit',1);
         xj = [x0; x(:,j); x1];
 
         ind = mediansmoothmex(xj,int32(w));
