56a1c1b2c974480ff9286eda19a11c39619618d5
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2010-09-05 00:30:39 +0000

    - NEW: gpc_cut_mex.c.
    git-svn-id: file:///svn_repos/mexfunctions.new@90 ad4b1c03-8ef8-444d-a533-6d794d5f2280

---
+++ gpc/gpc_cut_mex.c
@@ -0,0 +1,100 @@
+/* res = gpc_cut_mex(xy,n)
+ * Cut list of polygons using the General Polygon Clipper (GPC) library.
+ *
+ * URL: 
+ * http://www.cs.man.ac.uk/~toby/alan/software/gpc.html
+ *
+ * Compile on Linux: 
+ * mex -v COPTIMFLAGS=-O3 LDOPTIMFLAGS=-O3 -Igpc232 gpc_cut_mex.c gpc232/gpc.c
+ * 
+ * Compile on Windows
+ * mex -v OPTIMFLAGS=-O3 -Igpc232 gpc_cut_mex.c gpc232/gpc.c
+ *
+ * S. H. Muller, 2010/09/04
+ */
+
+#include "mex.h"
+#include "matrix.h"
+#include "math.h"
+#include "gpc.h"
+
+void mxExport(mxArray **L, gpc_polygon *p, int J)
+{
+    mwSize dims[2];
+    int *m, *M, Mc, *n, *N, Nc, i, j;
+    double *d;
+    
+    dims[0] = J;
+    L[2] = mxCreateNumericArray(1, dims, mxINT32_CLASS, mxREAL);
+    M = (int*) mxGetPr(L[2]);
+    for(j=0,m=M,Mc=0; j<J; ++j,++m) {
+        Mc += *m = p[j].num_contours;
+    }
+    dims[0] = Mc;
+    L[1] = mxCreateNumericArray(1, dims, mxINT32_CLASS, mxREAL);
+    N = (int*) mxGetPr(L[1]);
+    for(j=0,n=N,Nc=0; j<J; ++j) {
+        for(i=0; i<M[j]; ++i,++n) {
+            Nc += *n = p[j].contour[i].num_vertices;
+        }
+    }
+    dims[0] = 2; dims[1] = Nc;
+    L[0] = mxCreateNumericArray(2, dims, mxDOUBLE_CLASS, mxREAL);
+    d = mxGetPr(L[0]);
+    for(j=0,n=N; j<J; ++j) {
+        for(i=0; i<M[j]; ++i,++n) {
+            memcpy(d, p[j].contour[i].vertex, *n*2*sizeof(double));
+            d += *n*2;
+            if (p[j].hole && p[j].hole[i]) *n = -*n;
+        }
+    }
+}
+
+void cut(gpc_polygon *p, gpc_polygon *q, gpc_polygon *pi)
+{
+    gpc_polygon pp, qq;
+    gpc_polygon_clip(GPC_INT, p, q, pi);
+    gpc_polygon_clip(GPC_DIFF, p, q, &pp);
+    gpc_polygon_clip(GPC_DIFF, q, p, &qq);
+    if (p->hole) gpc_free_polygon(p);
+    if (q->hole) gpc_free_polygon(q);
+    *p = pp;
+    *q = qq;
+}
+
+void mexFunction(int nL, mxArray **L, int nR, const mxArray **R)
+{
+    int i,j;
+    const int m = mxGetNumberOfElements(R[1]);
+    const int *n, *N = (const int*) mxGetPr(R[1]);
+    const double *xy, *XY = mxGetPr(R[0]);
+    
+    int M = m*(m+1)/2;
+    gpc_vertex_list *v, *V = malloc(m*sizeof(gpc_vertex_list));
+    gpc_polygon *p, *q, *pi, *P = malloc(M*sizeof(gpc_polygon)), *Pi = P+m;
+    
+    for(i=0,xy=XY,v=V,p=P; i<m; xy+=2*N[i],++v,++p,++i) {
+        v->num_vertices = N[i];
+        v->vertex = (gpc_vertex*) xy;
+        p->num_contours = 1;
+        p->hole = NULL;
+        p->contour = v;
+    }
+    
+    for(i=0,p=P,q=p+1,pi=Pi; i<m-1; ++i,++p,++q) {
+        cut(p, q, pi++);
+        for(j=0; j<i; ++j) {
+            cut(Pi+j, q, pi++);
+        }
+    }
+    
+    printf("%d, %d\n", pi-Pi, m*(m-1)/2);
+    
+    mxExport(L, P, M);
+    
+    for(i=0,p=P; i<M; ++i,++p) {
+        gpc_free_polygon(p);
+    }
+    free(P);
+    free(V);
+}
--- gpc/gpc_tpatch_test.m
+++ gpc/gpc_tpatch_test.m
@@ -1,13 +1,11 @@
-function gpc_tpatch_test
-
 m = 20;
 phi = (0:m-1)*(2*pi/m);
 cph = cos(phi); sph = sin(phi);
 
-n1 = 3;
+n1 = 4;
 %x1 = 2*rand(n1,1);
 %y1 = 2*rand(n1,1);
-x1 = [1,1,2,2].';
+x1 = [1.2,1,2,2].';
 y1 = [1,1,1,1].';
 r1 = (1:n1).'./n1;
 
@@ -20,8 +18,39 @@ c = [0,0,1; 0,1,0; 1,0,0; 0,0,0];
 %c = jet(n1);
 a = 0.9*ones(n1,1);
 
-gpc_tpatch(xy,n,c,a);
+[XY,N,M] = gpc_cut_mex(xy,int32(n));
 
-xlim([0,3]);
-ylim([0,3]);
-axis square;
+%gpc_tpatch(xy,n,c,a);
+
+%xlim([0,3]);
+%ylim([0,3]);
+%axis square;
+
+jj = 0;
+ii = 0;
+J = length(M);
+c = 0;
+for j = 1:J
+    nc{j} = N(ii+(1:M(j)));
+    ii = ii+M(j);
+    
+    if M(j) ~= 0
+        c = c+1;
+    end
+    
+    for i = 1:M(j)
+        nji = abs(nc{j}(i));
+        xyc{i,j} = XY(:,jj+(1:nji));
+        jj = jj+nji;
+        
+        if nc{j}(i) > 0    
+            sty = {};
+        else
+            sty = {'LineStyle','--'};
+        end
+        line(xyc{i,j}(1,:),xyc{i,j}(2,:),c(ones(1,nji)),sty{:});
+    end
+end
+
+
+
