3f8bc1cf16b6081a2e00995e56bc4f6c862c8fd1
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2010-09-05 06:42:36 +0000

    - gpc_tpatch.m: Perform alpha blending exactly as contour cutting in gpc_cut_mex.c. Test cases   work now.
    git-svn-id: file:///svn_repos/mexfunctions.new@92 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- gpc/gpc_tpatch.m
+++ gpc/gpc_tpatch.m
@@ -1,67 +1,47 @@
 function h = gpc_tpatch(xy,n,c,a)
 
+[XY,N,M] = gpc_cut_mex(xy,int32(n));
+
 %bc = get(gca,'Color');
 bc = [1,1,1];
-S0 = struct('xy',cell(1,length(n)),'c',cell(1,length(n)));
-jj = 0;
+
+m = length(n);
+L = pow2(m)-1;
+C = zeros(L,3);
+p = 1;
 for j = 1:length(n)
-    S0(j).xy = xy(:,jj+(1:n(j)));
-    S0(j).c = alphablend(bc,c(j,:),a(j));
-    jj = jj+n(j);
+    C(p,:) = alphablend(bc,c(j,:),a(j));
+    pi = p+1;
+    for q = 1:p-1
+        C(pi,:) = alphablend(C(q,:),C(p,:),a(j));
+        pi = pi+1;
+    end
+    p = pi;
 end
 
-E = struct('xy',{},'c',{});
-S = E;
-for j = 1:length(S0)
-    sj = S0(j);
-    ih = 0;
-    for i = 1:length(S)
-        si = S(i);
-        [sji.xy,nji,xyi,ni,xyj,nj] = gpcmex(si.xy,sj.xy);
-        sji.c = alphablend(si.c,sj.c,a(j));
-            
-        nji, ni, nj
-        if length(ni) == 1
-            S(i).xy = xyi;
-        end
-        if length(nj) == 1
-            sj.xy = xyj;
-        end
-        if length(ni) == 0
-            %ih = i;
-        end
-        S = [S,split(sji,nji)];
-    end
-    if ih == 0
-        S = [S,sj];
-    else
-        S(ih) = sj;
-    end
-end
-
-for i = 1:length(S)
-    s = S(i);
-    z = i(ones(1,size(s.xy,2)));
-    patch(s.xy(1,:),s.xy(2,:),z,0,'FaceColor',s.c,'EdgeColor','none');
-end
+tpatch(XY,N,M,C);
 
 
 %--------------------------------------------------------------------------
 function c = alphablend(cb,ct,at)
 c = ct.*at+cb.*(1-at);
 
+
 %--------------------------------------------------------------------------
-function S = split(s,n)
+function tpatch(XY,N,M,C)
 jj = 0;
-S = struct('xy',{},'c',{});
-for j = 1:length(n)
-    if n(j) > 0
-        sj.xy = s.xy(:,jj+(1:n(j)));
-        sj.c = s.c;
-        S(end+1) = sj;
-        jj = jj+n(j);
-    else
-        jj = jj-n(j);
+ii = 0;
+J = length(M);
+for j = 1:J
+    nj = N(ii+(1:M(j)));
+    ii = ii+M(j);
+    cj = C(j,:);
+    
+    for i = 1:M(j)
+        nji = abs(nj(i));
+        xyji = XY(:,jj+(1:nji));
+        jj = jj+nji;
+        
+        patch(xyji(1,:),xyji(2,:),j(ones(1,nji)),0,'FaceColor',cj,'EdgeColor','none');
     end
 end
-
--- gpc/gpc_tpatch_test.m
+++ gpc/gpc_tpatch_test.m
@@ -16,42 +16,42 @@ for j = 1:n1
 end
 c = [0,0,1; 0,1,0; 1,0,0; 0,0,0; 1,1,1];
 %c = jet(n1);
-a = 0.9*ones(n1,1);
+a = 0.8*ones(n1,1);
 
-[XY,N,M] = gpc_cut_mex(xy,int32(n));
+gpc_tpatch(xy,n,c,a);
 
-%gpc_tpatch(xy,n,c,a);
+xlim([0,3]);
+ylim([0,3]);
+axis square;
 
-%xlim([0,3]);
-%ylim([0,3]);
-%axis square;
-
-jj = 0;
-ii = 0;
-J = length(M);
-c = 0;
-for j = 1:J
-    nc{j} = N(ii+(1:M(j)));
-    ii = ii+M(j);
-    
-    if M(j) ~= 0
-        c = c+1;
-    end
-    
-    for i = 1:M(j)
-        nji = abs(nc{j}(i));
-        xyc{i,j} = XY(:,jj+(1:nji));
-        jj = jj+nji;
-        
-        if nc{j}(i) > 0    
-            sty = {};
-        else
-            sty = {'LineStyle','--'};
-        end
-        patch(xyc{i,j}(1,:),xyc{i,j}(2,:),c(ones(1,nji)),c);
-        %line(xyc{i,j}(1,:),xyc{i,j}(2,:),c(ones(1,nji)),sty{:});
-    end
-end
-
+% [XY,N,M] = gpc_cut_mex(xy,int32(n));
+%
+% jj = 0;
+% ii = 0;
+% J = length(M);
+% c = 0;
+% for j = 1:J
+%     nc{j} = N(ii+(1:M(j)));
+%     ii = ii+M(j);
+%     
+%     if M(j) ~= 0
+%         c = c+1;
+%     end
+%     
+%     for i = 1:M(j)
+%         nji = abs(nc{j}(i));
+%         xyc{i,j} = XY(:,jj+(1:nji));
+%         jj = jj+nji;
+%         
+%         if nc{j}(i) > 0    
+%             sty = {};
+%         else
+%             sty = {'LineStyle','--'};
+%         end
+%         patch(xyc{i,j}(1,:),xyc{i,j}(2,:),c(ones(1,nji)),c);
+%         %line(xyc{i,j}(1,:),xyc{i,j}(2,:),c(ones(1,nji)),sty{:});
+%     end
+% end
+% 
 
 
