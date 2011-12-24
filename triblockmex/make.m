51beb7f7c0b401479b507a403920fcdbdeb1e1c8
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2011-12-24 00:19:07 +0000

    - NEW: triblockmex: Will become tri-block-diagonal solver.    * At the moment, just demonstrate linkage against lapack, implementing the      backslash operator for dense matrices without any simplifications.
    git-svn-id: file:///svn_repos/mexfunctions.new@98 ad4b1c03-8ef8-444d-a533-6d794d5f2280

---
+++ triblockmex/make.m
@@ -0,0 +1,13 @@
+% How to compile on Windows with standalone lapack:
+%
+% (1) Build lapack and blas on MSYS with MINGW gfortran.
+% (2) Run gnumex.
+% (3) On D:\matlab\gnumex\linkmex.pl, change "$linker = 'gcc -shared';" to
+%
+%     $linker = 'gfortran -shared';
+%
+% (4) Run make.m.
+
+%mex -v OPTIMFLAGS=-O3 -Ddgesv=dgesv_ triblockmex.c "C:\MinGW32-xy\lib\liblapack.a" "C:\MinGW32-xy\lib\libblas.a"
+
+mex -v OPTIMFLAGS=-O3 triblockmex.c -L"C:\PROGRA~1\MATLAB\R2007a\extern\lib\win32\lcc" -lmwlapack
---
+++ triblockmex/triblockmex.c
@@ -0,0 +1,40 @@
+/* X = triblockmex(A,B)
+ *
+ * S. H. Muller, 2011/12/23
+ */
+
+#include "mex.h"
+#include "matrix.h"
+#include "math.h"
+#include "string.h"
+
+extern void dgesv(int *N, int *NRHS, double *A, int *LDA, int *IPIV, 
+    double *B, int *LDB, int *INFO);
+
+void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
+{    
+    const mwSize ndims = mxGetNumberOfDimensions(R[1]);
+    const mwSize *dims = mxGetDimensions(R[1]);
+    const int npts = mxGetNumberOfElements(R[1]);
+    const double *A=mxGetPr(R[0]), *B=mxGetPr(R[1]);
+   
+    int N = dims[0], NRHS = dims[1], LDA = N, LDB = N, INFO = 0;
+    int *IPIV = malloc(N*sizeof(int));
+
+    double *a = malloc(N*N*sizeof(double));
+    double *b;
+    
+    L[0] = mxCreateNumericArray(ndims,dims,mxDOUBLE_CLASS,mxREAL);
+    b = mxGetPr(L[0]);
+    
+    memcpy(a,A,N*N*sizeof(double));
+    memcpy(b,B,npts*sizeof(double));
+    
+    dgesv(&N, &NRHS, a, &LDA, IPIV, b, &LDB, &INFO);
+    
+    //printf("ndims = %d, N = %d, NRHS = %d, npts = %d, INFO = %d\n", ndims, N, NRHS, npts, INFO);
+    
+    free(a);
+    free(IPIV);
+    
+}
