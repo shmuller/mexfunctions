e5eab86f467f609cb46bbd3ca7f6ea6a79549fd4
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2011-12-28 23:49:12 +0000

    - triblockmex.c: Modify arrays in place, overriding const directives. Do not allocate   any memory.
    git-svn-id: file:///svn_repos/mexfunctions.new@99 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- triblockmex/triblockmex.c
+++ triblockmex/triblockmex.c
@@ -16,25 +16,26 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
     const mwSize ndims = mxGetNumberOfDimensions(R[1]);
     const mwSize *dims = mxGetDimensions(R[1]);
     const int npts = mxGetNumberOfElements(R[1]);
-    const double *A=mxGetPr(R[0]), *B=mxGetPr(R[1]);
-   
+    double *A=mxGetData(R[0]), *B=mxGetData(R[1]);
+    int *IPIV=mxGetData(R[2]);
+    
     int N = dims[0], NRHS = dims[1], LDA = N, LDB = N, INFO = 0;
-    int *IPIV = malloc(N*sizeof(int));
+    //int *IPIV = malloc(N*sizeof(int));
 
-    double *a = malloc(N*N*sizeof(double));
-    double *b;
+    //double *a = malloc(N*N*sizeof(double));
+    //double *b;
     
-    L[0] = mxCreateNumericArray(ndims,dims,mxDOUBLE_CLASS,mxREAL);
-    b = mxGetPr(L[0]);
+    //L[0] = mxCreateNumericArray(ndims,dims,mxDOUBLE_CLASS,mxREAL);
+    //b = mxGetPr(L[0]);
     
-    memcpy(a,A,N*N*sizeof(double));
-    memcpy(b,B,npts*sizeof(double));
+    //memcpy(a,A,N*N*sizeof(double));
+    //memcpy(b,B,npts*sizeof(double));
     
-    dgesv(&N, &NRHS, a, &LDA, IPIV, b, &LDB, &INFO);
+    dgesv(&N, &NRHS, A, &LDA, IPIV, B, &LDB, &INFO);
     
     //printf("ndims = %d, N = %d, NRHS = %d, npts = %d, INFO = %d\n", ndims, N, NRHS, npts, INFO);
     
-    free(a);
-    free(IPIV);
+    //free(a);
+    //free(IPIV);
     
 }
