fefe484db4f06c46f3458bbeef33f95b3b378d44
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2012-01-18 12:39:30 +0000

    - quadfunmex.c: Implement parameters.
    git-svn-id: file:///svn_repos/mexfunctions.new@129 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- quadmex/quadfunmex.c
+++ quadmex/quadfunmex.c
@@ -15,7 +15,9 @@
 
 #include "gauss_legendre.h"
 
-typedef double (func)(void*);
+#define max(a,b) ((b) > (a) ? (b) : (a))
+
+typedef double (func)(double *);
 
 typedef struct {
     func *fun;
@@ -24,9 +26,8 @@ typedef struct {
     double *par;
 } intpar;
 
-double Fun(void *data)
+double Fun(double *par)
 {
-    double *par = data;
     double x=par[0], y=par[1], z=par[2];
     return x*y*y*z*z*z;
 }
@@ -36,7 +37,7 @@ double integrate(double x, void *data)
     intpar *IP = data;
     *IP->par = x;
     if (IP->fun != NULL) {
-        return IP->fun(IP+1);
+        return IP->fun((double*)(IP+1));
     } else {
         return gauss_legendre(IP->n, integrate, IP+1, IP->ab[0], IP->ab[1]);
     }
@@ -45,27 +46,57 @@ double integrate(double x, void *data)
 
 mxArray *gauss_legendre_fun(int nI, const int *d, const int *n, int nR, mxArray **R)
 {
-    int i;
+    int i, j, l, L=1, c, C, nP = nR-nI;
     mxArray *res;
-    double y;
-        
+    double *y;
+    double **P, **p;
+    
     intpar *ip, *IP = malloc(nI*sizeof(intpar)+nR*sizeof(double));
     double *ab, *par = (double*)(IP+nI);
     
+    // get parameters
+    if (nP > 0) {
+        P = malloc(2*nP*sizeof(double*));
+        p = P+nP;
+        for(j=0,C=0; j<nR; j++) {
+            for(i=0; i<nI; i++) if (j==d[i]-1) break;
+            if (i == nI) {
+                P[C] = mxGetData(R[j]);
+                l = mxGetNumberOfElements(R[j]);
+                if (l == 1) {
+                    par[j] = *P[C];  // write singleton parameters
+                } else {
+                    L = max(L,l);
+                    p[C] = par+j;
+                    C++;
+                }
+            }
+        }
+    }
+    
+    res = mxCreateNumericMatrix(L,1,mxDOUBLE_CLASS,mxREAL);
+    y = mxGetData(res);
+    
+    // get integration variables
     for(i=1,ip=IP; i<nI; i++,ip++) {
         ip->fun = NULL;
         ip->n = n[i];
-        ip->ab = mxGetData(R[i]);
-        ip->par = par+i-1;
+        ip->ab = mxGetData(R[d[i]-1]);
+        ip->par = par+d[i-1]-1;
     }
     ip->fun = Fun;
-    ip->par = par+i-1;
+    ip->par = par+d[i-1]-1;
+    ab = mxGetData(R[d[0]-1]);
     
-    ab = mxGetData(R[0]);
-    y = gauss_legendre(n[0], integrate, IP, ab[0], ab[1]);
+    // perform integrations
+    for(i=0; i<L; i++) {
+        for(c=0; c<C; c++) {
+            *p[c] = P[c][i];
+        }
+        *y++ = gauss_legendre(n[0], integrate, IP, ab[0], ab[1]);
+    }
     
-    res = mxCreateDoubleScalar(y);
-    
+    if (nP > 0) free(P);
     free(IP);
     
     return res;
