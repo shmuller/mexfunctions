18b55f97b46a95194c5926cd317d87d0ca6e7318
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2012-02-01 07:36:47 +0000

    - dblMaxwmex/dblMaxwmex.c: Prepare structure for incomplete integrals case.
    git-svn-id: file:///svn_repos/mexfunctions.new@140 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- dblMaxwmex/dblMaxwmex.c
+++ dblMaxwmex/dblMaxwmex.c
@@ -13,30 +13,42 @@
 
 #define STRLEN 1024
 
+#define SQRT2PI 2.50662827463100050241576528481105
+
 typedef real (func_r)(real, void *);
 
 typedef struct {
+    real x0;
+    real r0;
     func_r *f_r;
     void *data_r;
+    func *f_angle;
     real *data_angle;
-} fun_data;
+} intgrd_data;
 
-real one(real x, void *data)
+real one(real r, void *data)
 {
     return 1.;
 }
 
-real vrel(real x, void *data)
+real vrel(real r, void *data)
 {
-    double *v = data;
-    return x*(*v);
+    return r;
+}
+
+real Maxwellian(real x, real x0, real xt)
+{
+    x -= x0;
+    x /= xt;
+    return exp(-0.5*x*x)/(SQRT2PI*xt);
 }
 
 real intgrd(real x, void *data)
 {
-    fun_data *f = data;
-    *f->data_angle = x;
-    return f->f_r(x,f->data_r)*angle_int(f->data_angle);
+    intgrd_data *ID = data;
+    real r = hypot(x,ID->x0)*ID->r0;
+    *ID->data_angle = x;
+    return ID->f_r(r,ID->data_r)*ID->f_angle(ID->data_angle);
 }
 
 
@@ -52,23 +64,35 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
     double wt = hypot(*vt,*ut);
     
     int *IJ = mxGetData(R[4]);
-    int n = 6-IJ[0]-IJ[1];
+    int m = 6-IJ[0]-IJ[1];
     
-    double w0[3];
-    for(i=0; i<3; i++) w0[i]=u0[i]-v0[i];
+    double w0[] = {u0[0]-v0[0], u0[1]-v0[1], u0[2]-v0[2]};
+    double R0, z0, angle_data[4], *y;
     
-    double R0=hypot(w0[0],w0[1]), z0=w0[2];
+    intgrd_data ID = {0., 1., NULL, NULL, NULL, angle_data};
     
-    double angle_data[] = {0., 1., z0/wt, R0/wt};
- 
+    ID.f_r = vrel;
+    
     //atomic_desc D = get_atomic_desc("D", "CX", "");
+    //ID.f_r = (func_r*) sigmav;
+    //ID.data_r = &D;
     
-    //fun_data f = {(func_r*) sigmav, &D, x};
-    
-    fun_data f = {vrel, &wt, angle_data};
-    
-    double res = gauss_legendre(256, intgrd, &f, 0., 10.);
-    
-    L[0] = mxCreateDoubleScalar(res);
+    if (IJ[0] == 3 && IJ[1] == 3) {
+        R0 = hypot(w0[0],w0[1]); 
+        z0 = w0[2];
+        ID.r0 = wt;
+        ID.f_angle = angle_int;
+        angle_data[1] = 1.;
+        angle_data[2] = z0/wt;
+        angle_data[3] = R0/wt;
+        
+        L[0] = mxCreateNumericMatrix(1,1,mxDOUBLE_CLASS,mxREAL);
+        y = mxGetData(L[0]);
+        
+        *y = gauss_legendre(256, intgrd, &ID, 0., 10.);
+        
+    } else if (IJ[0] == 2 && IJ[1] == 3) {
+        
+    }
     
 }
