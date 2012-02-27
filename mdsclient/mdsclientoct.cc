baa2d312dee6c6b395aa00629b352709821e8d54
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2012-02-27 22:27:33 +0000

    - mdsclientoct.cc: Deal with scalar data types. Everything should work now!
    git-svn-id: file:///svn_repos/mexfunctions.new@163 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mdsclient/mdsclientoct.cc
+++ mdsclient/mdsclientoct.cc
@@ -63,67 +63,29 @@ void oct2mds(Descrip *D, const octave_value &in)
     oct2mds_dims(D, in);
     oct2mds_dtype(D, in);
 
-    if (in.is_string()) {
+    if (in.is_string()) 
+    {
         void *ptr = calloc(D->num+1,sizeof(char));
         memcpy(ptr,in.mex_get_data(),D->num);
 	mkDescrip(D, D->w_dtype, 0, NULL, 0, D->siz, ptr);
-    } else if (in.is_real_type()) {
+    } 
+    else if (in.is_real_type() || in.is_scalar_type()) 
+    {
+        // real or scalar complex
         D->ptr = in.mex_get_data();
-    } else if (in.is_double_type()) {
-        const ComplexNDArray tmp = in.complex_array_value();
-        D->ptr = (void*)tmp.data();
-    } else if (in.is_single_type()) {
-        const FloatComplexNDArray tmp = in.float_complex_array_value();
-        D->ptr = (void*)tmp.data();
+    } 
+    else if (in.is_double_type()) 
+    {
+        const ComplexNDArray t = in.complex_array_value();
+        D->ptr = (void*)t.data();
+    } 
+    else if (in.is_single_type()) 
+    {
+        const FloatComplexNDArray t = in.float_complex_array_value();
+        D->ptr = (void*)t.data();
     }
-    
-    /*
-    if (in.is_string()) {
-        const charNDArray tmp = in.char_array_value();
-        void *out = calloc(D->num+1,sizeof(char));
-        memcpy(out,tmp.data(),D->num);
-        mkDescrip(D, w_dtype_CSTRING, 0, NULL, 0, D->siz, out);
-    } else if (in.is_real_type()) {
-        if (in.is_double_type()) {
-  	    const NDArray tmp = in.array_value();
-            mkDescrip_data(D, w_dtype_DOUBLE, (void*)tmp.data());
-        } else if (in.is_single_type()) {
-            const FloatNDArray tmp = in.float_array_value();
-            mkDescrip_data(D, w_dtype_FLOAT, (void*)tmp.data());
-        } else if (in.is_uint8_type()) {
-            const uint8NDArray tmp = in.uint8_array_value();
-            mkDescrip_data(D, w_dtype_UCHAR, (void*)tmp.data());
-        } else if (in.is_int8_type()) {
-            const int8NDArray tmp = in.int8_array_value();
-            mkDescrip_data(D, w_dtype_CHAR, (void*)tmp.data());
-        } else if (in.is_uint16_type()) {
-            const uint16NDArray tmp = in.uint16_array_value();
-            mkDescrip_data(D, w_dtype_USHORT, (void*)tmp.data());
-        } else if (in.is_int16_type()) {
-            const int16NDArray tmp = in.int16_array_value();
-            mkDescrip_data(D, w_dtype_SHORT, (void*)tmp.data());
-        } else if (in.is_uint32_type()) {
-            const uint32NDArray tmp = in.uint32_array_value();
-            mkDescrip_data(D, w_dtype_ULONG, (void*)tmp.data());
-        } else if (in.is_int32_type()) {
-            const int32NDArray tmp = in.int32_array_value();
-            mkDescrip_data(D, w_dtype_LONG, (void*)tmp.data());
-        } else if (in.is_uint64_type()) {
-            const uint64NDArray tmp = in.uint64_array_value();
-            mkDescrip_data(D, w_dtype_ULONGLONG, (void*)tmp.data());
-        } else if (in.is_int64_type()) {
-            const int64NDArray tmp = in.int64_array_value();
-            mkDescrip_data(D, w_dtype_LONGLONG, (void*)tmp.data());
-        }
-    } else if (in.is_double_type()) {
-        const ComplexNDArray tmp = in.complex_array_value();
-        mkDescrip_data(D, w_dtype_COMPLEX_DOUBLE, (void*)tmp.data());
-    } else if (in.is_single_type()) {
-        const FloatComplexNDArray tmp = in.float_complex_array_value();
-        mkDescrip_data(D, w_dtype_COMPLEX, (void*)tmp.data());
-    }
-    */
 }
+
 
 void mds2oct(octave_value &out, const Descrip *D)
 {
@@ -138,69 +100,69 @@ void mds2oct(octave_value &out, const Descrip *D)
     switch (D->w_dtype) {
        case w_dtype_CSTRING: {
            dv(1) = D->num;
-           charNDArray tmp(dv);
-	   memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           charNDArray t(dv);
+	   memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_UCHAR: {
-           uint8NDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           uint8NDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_CHAR: {
-           int8NDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           int8NDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_USHORT: {
-           uint16NDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           uint16NDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_SHORT: {
-           int16NDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           int16NDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_ULONG: {
-           uint32NDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           uint32NDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_LONG: {
-           int32NDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           int32NDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_ULONGLONG: {
-           uint64NDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           uint64NDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_LONGLONG: {
-           int64NDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           int64NDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_FLOAT: {
-           FloatNDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           FloatNDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_COMPLEX: {
-           FloatComplexNDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           FloatComplexNDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_DOUBLE: {
-           NDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           NDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
        case w_dtype_COMPLEX_DOUBLE: {
-           ComplexNDArray tmp(dv);
-           memcpy((void*)tmp.data(), D->ptr, numbytes);
-           out = tmp; break;
+           ComplexNDArray t(dv);
+           memcpy((void*)t.data(), D->ptr, numbytes);
+           out = t; break;
        }
     }
 }
@@ -222,8 +184,7 @@ DEFUN_DLD(mdsclientmex, args, nargout, "MDSplus client")
 
     //sm_mdsopen(sock, "rcp", 132777);
 
-    Descrip l, *R;
-    
+    Descrip l, *R;    
     int nR = args.length();
 
     R = (Descrip*) malloc(nR*sizeof(Descrip));
