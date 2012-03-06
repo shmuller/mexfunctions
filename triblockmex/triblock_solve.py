110027012c77e62be125880614239a43ff3d0421
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2012-03-06 23:12:04 +0000

    - triblock_solve.py: Make save_restore and block_generator objects.
    git-svn-id: file:///svn_repos/mexfunctions.new@185 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- triblockmex/triblock_solve.py
+++ triblockmex/triblock_solve.py
@@ -12,7 +12,77 @@
 import numpy as np
 import triblock
 import h5py
-import os
+
+class save_restore:
+    """Save and restore blocks."""
+    def __init__(self, fname="tmp.h5", fmt="%03d"):
+        self.fname = fname
+        self.fmt = fmt
+    def open(self, mode="w"):
+        self.h = h5py.File(self.fname, mode)
+    def close(self):
+        self.h.close()
+    def save(self, j, x):
+        self.h[self.fmt % j] = x
+    def load(self, j):
+        return self.h[self.fmt % j][:]
+
+
+class block_generator:
+    """Generate blocks."""
+    def __init__(self, L, D, U):
+        M = D.shape[2]
+        self.D = D.swapaxes(1,2).copy()
+        self.L = L.swapaxes(1,2)[:,M/2:].copy()
+        self.U = U.copy()
+
+    def getL(self,j):
+        return self.L[j]
+    def getD(self,j):
+        return self.D[j]
+    def getU(self,j):
+        return self.U[j]
+
+
+def diagonalize(S,G,siz):
+    J, M = siz
+    IPIV = np.zeros(M,'i')
+
+    D1 = G.getD(0)
+    for j in range(J-1):
+        D2 = G.getD(j+1)
+        U2 = G.getU(j+1)
+        L1 = G.getL(j)
+        triblock.step(D1,U2,IPIV,L1,D2,1)
+        D1 = D2
+        S.save(j,U2)
+    
+    S.save(J-1,D1)
+
+
+def backsubst(S,b,siz):
+    J, M = siz
+    D1 = S.load(J-1)
+    b[J-1] = np.linalg.solve(D1,b[J-1])
+    for j in range(J-2,-1,-1):
+        U2 = S.load(j)
+        b[j] = -np.dot(U2.T,b[j+1])
+    return b
+
+
+def solve_otf(S,G,b):
+    siz = b.shape
+
+    S.open("w")
+    diagonalize(S,G,siz)
+    S.close()
+
+    S.open("r")
+    b = backsubst(S,b,siz)
+    S.close()
+    return b
+
+
 
 def solve_full(L,D,U,b):
     """Solve full block-tridiagonal system."""
@@ -58,48 +128,6 @@ def solve(L,D,U,b,is_half=0):
         return solve_half(L,D,U,b)
 
 
-def solve_otf_diagonalize(h,fmt,L,D,U,siz):
-    J, M = siz    
-    D1 = D[0].T.copy()
-    U = U.copy()
-
-    IPIV = np.zeros(M,'i')
-
-    for j in range(J-1):
-        U2 = U[j+1]
-        L1 = L[j].T[M/2:].copy()
-        D2 = D[j+1].T.copy()
-        triblock.step(D1,U2,IPIV,L1,D2,1)
-        D1 = D2
-        h[fmt(j)] = U2
-    
-    h[fmt(J-1)] = D1
-
-
-def solve_otf_backsubst(h,fmt,b,siz):
-    J, M = siz
-    D1 = h[fmt(J-1)][:]
-    b[J-1] = np.linalg.solve(D1,b[J-1])
-    for j in range(J-2,-1,-1):
-        U2 = h[fmt(j)][:]
-        b[j] = -np.dot(U2.T,b[j+1])
-    return b
-
-
-def solve_otf(L,D,U,b):
-    fmt = lambda j: "%03d" % j
-    siz = b.shape
-
-    h = h5py.File("tmp.h5","w")
-    solve_otf_diagonalize(h,fmt,L,D,U,siz)
-    h.close()
-
-    h = h5py.File("tmp.h5","r")        
-    b = solve_otf_backsubst(h,fmt,b,siz)
-    h.close()
-    return b
-
-
 if __name__ == "__main__":
     n, m = 2, 3
 
@@ -113,6 +141,9 @@ if __name__ == "__main__":
     U[:,:,n/2:] = 0.
     L[:,:,:n/2] = 0.
     b[:m-1,:] = 0.
+
+    S = save_restore("tmp.h5", "%03d")
+    G = block_generator(L,D,U)
 
     A = np.zeros((n*m,n*m))
 
@@ -132,7 +163,7 @@ if __name__ == "__main__":
 
     x3 = solve(L,D,U,b.copy(),1)
 
-    x4 = solve_otf(L,D,U,b.copy())
+    x4 = solve_otf(S,G,b.copy())
 
     xx = np.c_[x,x2.ravel(),x3.ravel(),x4.ravel()]
 
