e6b8d2fd3ce9c62c2d4dd0db455e83a328172191
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2012-03-06 16:59:38 +0000

    - triblockmex/triblock_solve.py: Add solve_otf() that will calculate the blocks   on the fly.
    git-svn-id: file:///svn_repos/mexfunctions.new@183 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- triblockmex/triblock_solve.py
+++ triblockmex/triblock_solve.py
@@ -1,46 +1,78 @@
+"""
+    Solve block-tridiagonal system Ax=b in place. L, D and U are 3D arrays 
+    of N block matrices, such that
+
+        | D1 U2               |
+    A = | L1 D2 ...           | 
+        |           L(N-1) DN | 
+
+    S. H. Muller, 2012/03/05
+"""
+
 import numpy as np
 import triblock
 
-def solve(L,D,U,b,is_half=0):
-    """
-    b = triblock_solve(L,D,U,b,is_half)
-       Solve block-tridiagonal system Ax=b in place. L, D and U are 3D arrays 
-       of N block matrices, such that
-
-            | D1 U2               |
-       A =  | L1 D2 ...           | 
-            |           L(N-1) DN | 
-
-       S. H. Muller, 2012/03/05
-    """
-
+def solve_full(L,D,U,b):
+    """Solve full block-tridiagonal system."""
     J, N, M = D.shape
+    IPIV = np.zeros(M,'i')
 
-    IPIV = np.zeros(M,dtype='i')
-    
+    D = np.concatenate((D,b[:,None,:]),axis=1).copy()
+    U = np.concatenate((U,np.zeros((J,1,M))),axis=1).copy()
+
+    for j in range(J-1):
+        U[j+1,-1] = D[j,-1]
+        triblock.step(D[j],U[j+1],IPIV,L[j],D[j+1])
+
+    b[J-1] = np.linalg.solve(D[J-1,:N].T,D[J-1,-1])
+    for j in range(J-2,-1,-1):
+        b[j] = U[j+1,-1] - np.dot(U[j+1,:N].T,b[j+1])
+    return b
+
+
+def solve_half(L,D,U,b):
+    """Solve full block-tridiagonal system."""
+    J, N, M = D.shape
+    IPIV = np.zeros(M,'i')
+
+    D = D.swapaxes(1,2).copy()
+    L = L.swapaxes(1,2)[:,M/2:].copy()
+    U = U.copy()
+
+    for j in range(J-1):
+        triblock.step(D[j],U[j+1],IPIV,L[j],D[j+1],1)
+
+    b[J-1] = np.linalg.solve(D[J-1],b[J-1])
+    for j in range(J-2,-1,-1):
+        b[j] = -np.dot(U[j+1].T,b[j+1])
+    return b
+
+
+def solve(L,D,U,b,is_half=0):
+    """Solve block-tridiagonal system."""
     if not is_half:
-        D = np.concatenate((D,b[:,None,:]),axis=1).copy()
-        U = np.concatenate((U,np.zeros((J,1,M))),axis=1).copy()
-
-        for j in range(J-1):
-            U[j+1,-1] = D[j,-1]
-            triblock.step(D[j],U[j+1],IPIV,L[j],D[j+1])
-
-        b[J-1] = np.linalg.solve(D[J-1,:N].T,D[J-1,-1])
-        for j in range(J-2,-1,-1):
-            b[j] = U[j+1,-1] - np.dot(U[j+1,:N].T,b[j+1])
-
+        return solve_full(L,D,U,b)
     else:
-        D = D.swapaxes(1,2).copy()
-        L = L.swapaxes(1,2)[:,M/2:].copy()
+        return solve_half(L,D,U,b)
 
-        for j in range(J-1):
-            triblock.step(D[j],U[j+1],IPIV,L[j],D[j+1],1)
 
-        b[J-1] = np.linalg.solve(D[J-1],b[J-1])
-        for j in range(J-2,-1,-1):
-            b[j] = -np.dot(U[j+1].T,b[j+1])
+def solve_otf(L,D,U,b):
+    J, M = b.shape
+    IPIV = np.zeros(M,'i')
 
+    D1 = D[0].T.copy()
+    U = U.copy()
+
+    for j in range(J-1):
+        U2 = U[j+1]
+        L1 = L[j].T[M/2:].copy()
+        D2 = D[j+1].T.copy()
+        triblock.step(D1,U2,IPIV,L1,D2,1)
+        D1 = D2
+
+    b[J-1] = np.linalg.solve(D1,b[J-1])
+    for j in range(J-2,-1,-1):
+        b[j] = -np.dot(U[j+1].T,b[j+1])
     return b
 
 
@@ -76,7 +108,9 @@ if __name__ == "__main__":
 
     x3 = solve(L,D,U,b.copy(),1)
 
-    xx = np.c_[x,x2.ravel(),x3.ravel()]
+    x4 = solve_otf(L,D,U,b.copy())
+
+    xx = np.c_[x,x2.ravel(),x3.ravel(),x4.ravel()]
 
     print xx
 
