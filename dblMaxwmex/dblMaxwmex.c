aefee54bd7e95ed8dc83746a8be2c7faf8ea9469
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2012-03-28 12:34:00 +0000

    - dblMaxwmex.c: Separate generic from mex code.
    git-svn-id: file:///svn_repos/mexfunctions.new@197 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- dblMaxwmex/dblMaxwmex.c
+++ dblMaxwmex/dblMaxwmex.c
@@ -67,28 +67,23 @@ int compare_r(SM_REAL *DATA, const unsigned *A, const unsigned *B)
 
 #define Nr 32
 
-void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
+void dblMaxw(char *f_r, double *vt, double *v0, double *ut, double *u0,
+    int *IJ, int *DI, double **V, double **U, double *Y)
 {
     int i, j, k, l;
 
-    char f_r[STRLEN];
-    mxGetString(*R++,f_r,STRLEN);
+    double *y;
 
-    double *vt = mxGetData(*R++);
-    double *v0 = mxGetData(*R++);
-    double *ut = mxGetData(*R++);
-    double *u0 = mxGetData(*R++);
-    
     double wt = hypot(*vt,*ut);
     double rM = 10.*wt;
 
-    int *IJ = mxGetData(*R++);
-    int m = 6-IJ[0]-IJ[1];
-
     double w0[] = {u0[0]-v0[0], u0[1]-v0[1], u0[2]-v0[2]};
-    double data_angle[5], *y;
-    
+
+    double data_angle[5];
     intgrd_data ID = {0., 1., NULL, NULL, AngleInt3_, data_angle};
+
+    int m = 6-IJ[0]-IJ[1], N=1;
+    for(i=0; i<m; i++) N *= DI[i];
 
     atomic_desc D;
 
@@ -108,128 +103,106 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
 
     if (IJ[1] == 3) {
         if (IJ[0] == 3) {
-            L[0] = mxCreateNumericMatrix(1,1,mxDOUBLE_CLASS,mxREAL);
-            y = mxGetData(L[0]);
-
             R0 = 0.;
             Rt = wt;
             z0 = sqrt(w0[0]*w0[0]+w0[1]*w0[1]+w0[2]*w0[2]);
             zt = wt;
                 
-            *y = gauss_legendre(Nr, intgrd, &ID, 0., rM);
+            *Y = gauss_legendre(Nr, intgrd, &ID, 0., rM);
         
-        } else if (IJ[0] == 2) {
-               
-            double *v3 = mxGetData(R[0]);
-            int ndims = mxGetNumberOfDimensions(R[0]);
-            mwSize *dims = mxGetDimensions(R[0]);
-            int N = mxGetNumberOfElements(R[0]);
-
-            L[0] = mxCreateNumericArray(ndims,dims,mxDOUBLE_CLASS,mxREAL);
-            y = mxGetData(L[0]);
+        } else if (IJ[0] == 2) {   
+            double *v3 = V[0];
 
             R0 = hypot(w0[0],w0[1]);
             Rt = wt;
             zt = *ut;
 
-            for(i=0; i<N; i++) {
+            for(i=0,y=Y; i<N; i++) {
                 z0 = u0[2]-v3[i];
                 *y++ = gauss_legendre(Nr, intgrd, &ID, 0., rM);
-                //*y++ = fM(v3[i]-v0[2],*vt)*gauss_legendre(Nr, intgrd, &ID, 0., rM);
+            }
+
+            for(i=0,y=Y; i<N; i++) {
+                *y++ *= fM(v3[i]-v0[2],*vt);
             }
 
         } else if (IJ[0] == 1) {
-            double *v1 = mxGetData(R[0]);
-            double *v2 = mxGetData(R[1]);
-            int N1 = mxGetNumberOfElements(R[0]);
-            int N2 = mxGetNumberOfElements(R[1]);
-
-            mwSize dims[] = {N1,N2};
-            L[0] = mxCreateNumericArray(2,dims,mxDOUBLE_CLASS,mxREAL);
-            y = mxGetData(L[0]);
-
-            double dx, dy;
+            double *v1 = V[0], *v2 = V[1];
+            double dx, dy, fM1, fM2;
 
             Rt = *ut;
             z0 = w0[2];
             zt = wt;
 
-            for(j=0; j<N2; j++) {
+            for(j=0,y=Y; j<DI[1]; j++) {
                 dy = u0[1]-v2[j];
-                for(i=0; i<N1; i++) {
+                for(i=0; i<DI[0]; i++) {
                     dx = u0[0]-v1[i];
                     R0 = hypot(dx,dy);
                     *y++ = gauss_legendre(Nr, intgrd, &ID, 0., rM);
                 }
             }
+
+            for(j=0,y=Y; j<DI[1]; j++) {
+                fM2 = fM(v2[j]-v0[1],*vt);
+                for(i=0; i<DI[0]; i++) {
+                    fM1 = fM2*fM(v1[i]-v0[0],*vt);
+                    *y++ *= fM1;
+                }
+            }
+
         }
 
     } else if (IJ[1] == 2 && IJ[0] == 2) {
-        double *v3 = mxGetData(R[0]);
-        double *u3 = mxGetData(R[1]);
-        int N1 = mxGetNumberOfElements(R[0]);
-        int N2 = mxGetNumberOfElements(R[1]);
-
-        mwSize dims[] = {N1,N2};
-        L[0] = mxCreateNumericArray(2,dims,mxDOUBLE_CLASS,mxREAL);
-        y = mxGetData(L[0]);
-
+        double *v3 = V[0], *u3 = U[0];
+        double fM1, fM2;
+        
         ID.f_angle = AngleInt2;
         R0 = hypot(w0[0],w0[1]);
         Rt = wt;
 
-        for(j=0; j<N2; j++) {
-            for(i=0; i<N1; i++) {
-                ID.x0 = u3[j]-v3[i];
-                *y++ = gauss_legendre(Nr, intgrd, &ID, 0., rM);
+        for(j=0,y=Y; j<DI[1]; j++) for(i=0; i<DI[0]; i++) {
+            ID.x0 = u3[j]-v3[i];
+            *y++ = gauss_legendre(Nr, intgrd, &ID, 0., rM);
+        }
+
+        for(j=0,y=Y; j<DI[1]; j++) {
+            fM2 = fM(u3[j]-u0[2],*ut);
+            for(i=0; i<DI[0]; i++) {
+                fM1 = fM2*fM(v3[i]-v0[2],*vt);
+                *y++ *= fM1;
             }
         }
 
     } else if (IJ[1] == 1 && IJ[0] == 1) {
-        double *v1 = mxGetData(R[0]);
-        double *v2 = mxGetData(R[1]);
-        double *u1 = mxGetData(R[2]);
-        double *u2 = mxGetData(R[3]);
-        int N1 = mxGetNumberOfElements(R[0]);
-        int N2 = mxGetNumberOfElements(R[1]);
-        int N3 = mxGetNumberOfElements(R[2]);
-        int N4 = mxGetNumberOfElements(R[3]);
-        int N = N1*N2*N3*N4;
-
-        mwSize dims[] = {N1,N2,N3,N4};
-        L[0] = mxCreateNumericArray(4,dims,mxDOUBLE_CLASS,mxREAL);
-        y = mxGetData(L[0]);
+        double *v1 = V[0], *v2 = V[1], *u1 = U[0], *u2 = U[1];
+        double val, *ys, fM1, fM2, fM3, fM4;
 
         unsigned *seq = malloc(N*sizeof(unsigned));
-        unsigned *s = seq;
-
-        unsigned idx = 0;
+        unsigned idx, *s;
 
         ID.f_angle = AngleInt1;
         R0 = w0[2];
         Rt = wt;
 
-        for(l=0; l<N4; l++) for(k=0; k<N3; k++) {
-            for(j=0; j<N2; j++) for(i=0; i<N1; i++) {
+        for(l=0,y=Y,s=seq,idx=0; l<DI[3]; l++) for(k=0; k<DI[2]; k++) {
+            for(j=0; j<DI[1]; j++) for(i=0; i<DI[0]; i++) {
                 *y++ = hypot(u1[k]-v1[i],u2[l]-v2[j]);
                 *s++ = idx++;
             }
         }
 
-        y = mxGetData(L[0]);
-
         /*
-        for(i=0; i<N; i++) {
+        for(i=0,y=Y; i<N; i++) {
             ID.x0 = *y;
             *y++ = gauss_legendre(Nr, intgrd, &ID, 0., rM);
         }
         */
 
-        qsort_r(seq,N,sizeof(unsigned),y,compare_r);
+        qsort_r(seq,N,sizeof(unsigned),Y,compare_r);
 
-        double val, *ys;
         for(i=N,s=seq,ID.x0=-1.; i--; ) {
-            ys = y + *s++;
+            ys = Y + *s++;
             if (ID.x0 != *ys) {
                 ID.x0 = *ys;
                 val = gauss_legendre(Nr, intgrd, &ID, 0., rM);
@@ -237,14 +210,13 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
             *ys = val;
         }
 
-        double fM1,fM2,fM3,fM4;
-        for(l=0; l<N4; l++) {
+        for(l=0,y=Y; l<DI[3]; l++) {
             fM4 = fM(u2[l]-u0[1],*ut);
-            for(k=0; k<N3; k++) {
+            for(k=0; k<DI[2]; k++) {
                 fM3 = fM4*fM(u1[k]-u0[0],*ut);
-                for(j=0; j<N2; j++) {
+                for(j=0; j<DI[1]; j++) {
                     fM2 = fM3*fM(v2[j]-v0[1],*vt);
-                    for(i=0; i<N1; i++) {
+                    for(i=0; i<DI[0]; i++) {
                         fM1 = fM2*fM(v1[i]-v0[0],*vt);
                         *y++ *= fM1;
                     }
@@ -256,4 +228,41 @@ void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
     }
     
 }
+
+void mexFunction(int nL, mxArray *L[], int nR, const mxArray *R[])
+{
+    int i;
+    char f_r[STRLEN];
+    mxGetString(*R++,f_r,STRLEN);
+
+    double *vt = mxGetData(*R++);
+    double *v0 = mxGetData(*R++);
+    double *ut = mxGetData(*R++);
+    double *u0 = mxGetData(*R++);
+    
+    int *IJ = mxGetData(*R++);
+    int mV = 3-IJ[0], mU = 3-IJ[1], m = mV+mU;
+    
+    double **VU = (m==0) ? NULL : malloc(m*(sizeof(double*)+sizeof(int)));
+    double **V = VU, **U=VU+mV;
+    int *DI = VU+m;
+
+    for(i=0; i<m; i++) {
+        VU[i] = mxGetData(R[i]);
+        DI[i] = mxGetNumberOfElements(R[i]);
+    }
+
+    if (m == 0) {
+        L[0] = mxCreateNumericMatrix(1,1,mxDOUBLE_CLASS,mxREAL);
+    } else {
+        L[0] = mxCreateNumericArray(m,DI,mxDOUBLE_CLASS,mxREAL);
+    }
+    double *Y = mxGetData(L[0]);
+
+    dblMaxw(f_r, vt, v0, ut, u0, IJ, DI, V, U, Y);
+
+    if (VU) free(VU);
+}
+
+
 
