a2dc973f011f50127e833db06c87e23b18525a8d
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2012-09-18 10:09:38 +0000

    - pqueue2.c: Start implementing double queues.
    git-svn-id: file:///svn_repos/mexfunctions.new@223 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- mediansmoothmex/pqueue2.c
+++ mediansmoothmex/pqueue2.c
@@ -2,56 +2,126 @@
 
 #include "pqueue.h"
 
+#define PQUEUE2
+
 #ifndef PQUEUE2
 
 void *pqueue2_init(size_t n, void *cmppri, 
         void *getpri, void *setpri, void *getpos, void *setpos) 
 {
-    pqueue2_t *q = malloc(sizeof(pqueue2_t));
-    q->cmppri = cmppri;
-    q->q = pqueue_init(n, cmppri, getpri, setpri, getpos, setpos);
-    return q;
+    pqueue2_t *Q = malloc(sizeof(pqueue2_t));
+    Q->cmppri = cmppri;
+    Q->q = pqueue_init(n, cmppri, getpri, setpri, getpos, setpos);
+    return Q;
 }
 
-void pqueue2_free(pqueue2_t *q) {
-    pqueue_free(q->q);
-    free(q);
+void pqueue2_free(pqueue2_t *Q) {
+    pqueue_free(Q->q);
+    free(Q);
 }
 
-void *pqueue2_replace_head(pqueue2_t *q, void *d) {
-    return pqueue_replace_head(q->q, d);
+void *pqueue2_replace_head(pqueue2_t *Q, void *d) {
+    return pqueue_replace_head(Q->q, d);
 }
 
-void pqueue2_replace_with_higher(pqueue2_t *q, void *n, void *d) {
-    pqueue_replace_with_higher(q->q, n, d);
+void pqueue2_replace_with_higher(pqueue2_t *Q, void *n, void *d) {
+    pqueue_replace_with_higher(Q->q, n, d);
 }
 
-int pqueue2_insert(pqueue2_t *q, void *d) {
-    return pqueue_insert(q->q, d);
+int pqueue2_insert(pqueue2_t *Q, void *d) {
+    return pqueue_insert(Q->q, d);
 }
 
-void pqueue2_update(pqueue2_t *q, int cmp, void *d) {
-    pqueue_update(q->q, cmp, d);
+void pqueue2_update(pqueue2_t *Q, int cmp, void *d) {
+    pqueue_update(Q->q, cmp, d);
 }
 
-void *pqueue2_pop(pqueue2_t *q) {
-    return pqueue_pop(q->q);
+void *pqueue2_pop(pqueue2_t *Q) {
+    return pqueue_pop(Q->q);
 }
 
-int pqueue2_remove(pqueue2_t *q, void *d) {
-    return pqueue_remove(q->q, d);
+int pqueue2_remove(pqueue2_t *Q, void *d) {
+    return pqueue_remove(Q->q, d);
 }
 
-void *pqueue2_peek(pqueue2_t *q) {
-    return pqueue_peek(q->q);
+void *pqueue2_peek(pqueue2_t *Q) {
+    return pqueue_peek(Q->q);
 }
 
-void pqueue2_print(pqueue2_t *q, void *out, void *print) {
-    pqueue_print(q->q, out, print);
+void pqueue2_print(pqueue2_t *Q, void *out, void *print) {
+    pqueue_print(Q->q, out, print);
 }
 
 
 #else
+
+typedef struct {
+    void *head;
+    void *bulk;
+} dbl_queue_t;
+
+void *pqueue2_init(size_t n, void *cmppri, 
+        void *getpri, void *setpri, void *getpos, void *setpos) 
+{
+    static const int m = 16;
+    pqueue2_t *Q = malloc(sizeof(pqueue2_t));
+    Q->cmppri = cmppri;
+
+    dbl_queue_t *q = malloc(sizeof(dbl_queue_t));
+    q->head = pqueue_init(m, cmppri, getpri, setpri, getpos, setpos);
+    q->bulk = pqueue_init(n, cmppri, getpri, setpri, getpos, setpos);
+    Q->q = q;
+    return Q;
+}
+
+void pqueue2_free(pqueue2_t *Q) {
+    dbl_queue_t *q = Q->q;
+    pqueue_free(q->bulk);
+    pqueue_free(q->head);
+    free(q);
+    free(Q);
+}
+
+void *pqueue2_replace_head(pqueue2_t *Q, void *d) {
+    dbl_queue_t *q = Q->q;
+    return pqueue_replace_head(q->bulk, d);
+}
+
+void pqueue2_replace_with_higher(pqueue2_t *Q, void *n, void *d) {
+    dbl_queue_t *q = Q->q;
+    pqueue_replace_with_higher(q->bulk, n, d);
+}
+
+int pqueue2_insert(pqueue2_t *Q, void *d) {
+    dbl_queue_t *q = Q->q;
+    return pqueue_insert(q->bulk, d);
+}
+
+void pqueue2_update(pqueue2_t *Q, int cmp, void *d) {
+    dbl_queue_t *q = Q->q;
+    pqueue_update(q->bulk, cmp, d);
+}
+
+void *pqueue2_pop(pqueue2_t *Q) {
+    dbl_queue_t *q = Q->q;
+    return pqueue_pop(q->bulk);
+}
+
+int pqueue2_remove(pqueue2_t *Q, void *d) {
+    dbl_queue_t *q = Q->q;
+    return pqueue_remove(q->bulk, d);
+}
+
+void *pqueue2_peek(pqueue2_t *Q) {
+    dbl_queue_t *q = Q->q;
+    return pqueue_peek(q->bulk);
+}
+
+void pqueue2_print(pqueue2_t *Q, void *out, void *print) {
+    dbl_queue_t *q = Q->q;
+    pqueue_print(q->bulk, out, print);
+}
+
 
 #endif
 
