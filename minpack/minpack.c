e76842f60218df65218dafd97de79c8cbcbd3197
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2013-05-15 07:54:52 +0000

    - minpack/minpack.*: Add comments - NEW: odepack: Compile double version. No Python wrappers yet.
    git-svn-id: file:///svn_repos/mexfunctions.new@261 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- minpack/minpack.c
+++ minpack/minpack.c
@@ -1,3 +1,11 @@
+/* minpack.c
+ *
+ * Wrap minpack Fortran subroutines lmdif and lmdif1 to the
+ * (func*, data*) interface.
+ *
+ * S. H. Muller, 2012
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,6 +13,7 @@
 
 #include "minpack.h"
 
+// signature of minpack Fortran functions
 extern void lmdif_(void *fcn, int *m, int *n, double *x, double *fvec,
     double *ftol, double *xtol, double *gtol, int *maxfev, double *epsfcn,
     double *diag, int *mode, double *factor, int *nprint, int *info, int *nfev,
@@ -14,6 +23,8 @@ extern void lmdif_(void *fcn, int *m, int *n, double *x, double *fvec,
 extern void lmdif1_(void *fcn, int *m, int *n, double *x, double *fvec,
     double *tol, int *info, int *iwa, double *wa, int *lwa);
 
+
+// global container to pass data into function with Fortran call signature
 typedef struct {
     func *f;
     data *D;
@@ -21,17 +32,23 @@ typedef struct {
 
 container CONTAINER;
 
+
+// simple wrapper with Fortran call signature, obtaining data via CONTAINER
 static int wrapper(int *m, int *n, double *x, double *fvec, int *iflag)
 {
     func *f = CONTAINER.f;
     data *D = CONTAINER.D;
 
+    // hook up Fortran parameter and results arrays to D
     D->P = x;
     D->y = fvec;
+
+    // evaluate function
     f(D);
     return 0;
 }
 
+// wrapper with do_var mask
 static int wrapper_mask(int *m, int *n, double *x, double *fvec, int *iflag)
 {
     func *f = CONTAINER.f;
@@ -41,8 +58,10 @@ static int wrapper_mask(int *m, int *n, double *x, double *fvec, int *iflag)
     for (i=0; i < D->n; ++i) {
         do_var = D->do_var[i];
         if (do_var == 1) {
+            // update parameters that should be varied
             D->P[i] = *x++;
         } else if (do_var == 2) {
+            // if logarithmic fit, transform back from logarithm
             D->P[i] = exp(*x++);
         }
     }
@@ -56,13 +75,17 @@ int leastsq(func *f, data *D)
 {
     int i, m = D->m, n = D->n, do_var;
     
+    // if do_var, update n to the actual number of varied parameters
     if (D->do_var) for (i=0, n=0; i < D->n; ++i) if (D->do_var[i]) ++n;
     
+    // hook up Fortran results array from D->y
     double *fvec = D->y;
     
+    // default parameters (hard coded from lmdif1)
     double ftol = 1.49012e-8, xtol = 1.49012e-8, gtol = 0.0, epsfcn = 0.0, factor = 1.0e2;
     int maxfev = 200*(n+1), mode = 1, nprint = 0, info = 99, nfev = -1, ldfjac = m;
 
+    // allocate workspace in one big chunk
     void *mem = malloc((m*n + 6*n + m)*sizeof(double) + n*sizeof(int));
     double *x = mem;
     double *diag = x + n;
@@ -74,24 +97,30 @@ int leastsq(func *f, data *D)
     double *wa4 = wa3 + n;
     int *ipvt = (int*)(wa4 + m);
 
+    // initialize data carrier into Fortran signature wrappers
     CONTAINER.f = f;
     CONTAINER.D = D;
 
     if (D->do_var) {
+        // with do_var: fill x vector from D->P (starting guesses)
         for (i=0; i < D->n; ++i) {
             do_var = D->do_var[i];
             if (do_var == 1) {
+                // add parameter that should be varied
                 *x++ = D->P[i];
             } else if (do_var == 2) {
+                // do log transformation for log-varied parameters
                 *x++ = log(D->P[i]);
             }
         }
         x = mem;
 
+        // perform fitting
         lmdif_(wrapper_mask, &m, &n, x, fvec, &ftol, &xtol, &gtol, &maxfev, &epsfcn, 
             diag, &mode, &factor, &nprint, &info, &nfev, fjac, &ldfjac, ipvt, qtf, 
             wa1, wa2, wa3, wa4);
 
+        // undo transformations and store result in D->P
         for (i=0; i < D->n; ++i) {
             do_var = D->do_var[i];
             if (do_var == 1) {
@@ -102,19 +131,25 @@ int leastsq(func *f, data *D)
         }
 
     } else {
+        // no do_var: link x to D->P and perform fitting
         x = D->P;
         lmdif_(wrapper, &m, &n, x, fvec, &ftol, &xtol, &gtol, &maxfev, &epsfcn, 
             diag, &mode, &factor, &nprint, &info, &nfev, fjac, &ldfjac, ipvt, qtf, 
             wa1, wa2, wa3, wa4);
+
+        // link D->P back to x returned by lmdif (not the same memory as before!)
         D->P = x;
     }
 
+    // link back output vector
     D->y = fvec;
     
     free(mem);
     return 0;
 }
 
+
+// wrapper using simple interface lmdif1 (does not implement do_var)
 int leastsq1(func *f, data *D)
 {
     double tol = 1.49012e-8;
--- minpack/minpack.h
+++ minpack/minpack.h
@@ -1,14 +1,21 @@
+/* minpack.h
+ *
+ * Wrap minpack Fortran subroutines lmdif and lmdif1 to the 
+ * (func*, data*) interface.
+ *
+ * S. H. Muller, 2012
+ */
 
 typedef struct {
-    int n;
-    int m;
-    double *P;
-    int *do_var;
-    double *x;
-    double *y;
-    double *ydata;
-    double *w;
-    double *a;
+    int n;          // length of P, do_var
+    int m;          // length of x, y, ydata, w, a
+    double *P;      // parameter vector to be optimized
+    int *do_var;    // bool array to switch off optimization for some parameters
+    double *x;      // independent coordinate of data sites
+    double *y;      // vector to hold the result of function evaluations
+    double *ydata;  // data values
+    double *w;      // weights (not implemented)
+    double *a;      // parameter for fits allowing linear variations of P
 } data;
 
 typedef void (func)(data *D);
