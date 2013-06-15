e41d27964fb6b7fc54316f5cda074f33711f0c9f
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2013-06-15 12:33:11 +0000

    - boost.python/fitfun_boost_numpy.cpp: Add default arguments support   after hacking boost::numpy by adding a default constructor to   np::ndarray. Still unclear how to pass keyword arguments.
    git-svn-id: file:///svn_repos/mexfunctions.new@299 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- fitfun/python/boost.python/fitfun_boost_numpy.cpp
+++ fitfun/python/boost.python/fitfun_boost_numpy.cpp
@@ -6,7 +6,10 @@
 namespace p = boost::python;
 namespace np = boost::numpy;
 
-void parse_args(data *D, np::ndarray &P, np::ndarray &x, np::ndarray &y)
+np::ndarray None;
+
+void parse_args(data *D, np::ndarray &P, np::ndarray &x, np::ndarray &y, 
+                         np::ndarray &ydata, np::ndarray &a)
 {
     D->P = (double*) P.get_data();
     D->n = P.shape(0);
@@ -15,60 +18,59 @@ void parse_args(data *D, np::ndarray &P, np::ndarray &x, np::ndarray &y)
     D->m = x.shape(0);
 
     D->y = (double*) y.get_data();
-}
 
-void parse_args_a(data *D, np::ndarray &P, np::ndarray &x, np::ndarray &y, 
-                           np::ndarray &a)
-{
-    parse_args(D, P, x, y);
-    D->a = (double*) a.get_data();
-}
-
-void parse_args_ydata(data *D, np::ndarray &P, np::ndarray &x, np::ndarray &y, 
-                               np::ndarray &ydata)
-{
-    parse_args(D, P, x, y);
-    D->ydata = (double*) ydata.get_data();
+    if (ydata != None) D->ydata = (double*) ydata.get_data();
+    if (a != None) D->a = (double*) a.get_data();
 }
 
 
-np::ndarray meth_e2(np::ndarray &P, np::ndarray &x, np::ndarray &y)
+np::ndarray meth_e2(np::ndarray &P, np::ndarray &x, np::ndarray &y, 
+                    np::ndarray &ydata=None, np::ndarray &a=None)
 {
     data D = {0};
-    parse_args(&D, P, x, y);
+    parse_args(&D, P, x, y, ydata, a);
     e2(&D);
     return y;
 }
 
-np::ndarray meth_e2_diff(np::ndarray &P, np::ndarray &x, np::ndarray &y, np::ndarray &ydata)
+np::ndarray meth_e2_diff(np::ndarray &P, np::ndarray &x, np::ndarray &y, 
+                         np::ndarray &ydata=None, np::ndarray &a=None)
 {
     data D = {0};
-    parse_args_ydata(&D, P, x, y, ydata);
+    parse_args(&D, P, x, y, ydata, a);
     e2_diff(&D);
     return y;
 }
 
-double meth_e2_rms(np::ndarray &P, np::ndarray &x, np::ndarray &y, np::ndarray &ydata)
+double meth_e2_rms(np::ndarray &P, np::ndarray &x, np::ndarray &y, 
+                   np::ndarray &ydata=None, np::ndarray &a=None)
 {
     data D = {0};
-    parse_args_ydata(&D, P, x, y, ydata);
+    parse_args(&D, P, x, y, ydata, a);
     return e2_rms(&D);
 }
 
-np::ndarray meth_e2_fit(np::ndarray &P, np::ndarray &x, np::ndarray &y, np::ndarray &ydata)
+np::ndarray meth_e2_fit(np::ndarray &P, np::ndarray &x, np::ndarray &y, 
+                        np::ndarray &ydata=None, np::ndarray &a=None)
 {
     data D = {0};
-    parse_args_ydata(&D, P, x, y, ydata);
+    parse_args(&D, P, x, y, ydata, a);
     e2_fit(&D);
     return P;
 }
 
+BOOST_PYTHON_FUNCTION_OVERLOADS(meth_e2_overloads, meth_e2, 3, 5)
+BOOST_PYTHON_FUNCTION_OVERLOADS(meth_e2_diff_overloads, meth_e2_diff, 3, 5)
+BOOST_PYTHON_FUNCTION_OVERLOADS(meth_e2_rms_overloads, meth_e2_rms, 3, 5)
+BOOST_PYTHON_FUNCTION_OVERLOADS(meth_e2_fit_overloads, meth_e2_fit, 3, 5)
+
 
 BOOST_PYTHON_MODULE(fitfun_boost_numpy) {
     np::initialize();
-    p::def("e2", meth_e2);
-    p::def("e2_diff", meth_e2_diff);
-    p::def("e2_rms", meth_e2_rms);
-    p::def("e2_fit", meth_e2_fit);
+
+    p::def("e2", meth_e2, meth_e2_overloads());
+    p::def("e2_diff", meth_e2_diff, meth_e2_diff_overloads());
+    p::def("e2_rms", meth_e2_rms, meth_e2_rms_overloads());
+    p::def("e2_fit", meth_e2_fit, meth_e2_fit_overloads());
 }
 
