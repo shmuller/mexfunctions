d1d4048dab2439193c5d5e9028ca6aa9de7871b5
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2013-10-21 10:18:16 +0000

    pppack/ppualder.f: New efficient algorithm w/o additional storage
    git-svn-id: file:///svn_repos/mexfunctions.new@340 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- pppack/ppualder.f
+++ pppack/ppualder.f
@@ -1,33 +1,30 @@
       subroutine ppualder( break, coef, l, k, d, x, m, jderiv, y )
-      integer jderiv,l,k,d,m,   i,dd,j,mm,ndummy,n,kmax,ifac(10)
-      real break(l+1),coef(d,k,l),x(m),y(d,m),   f,h,hf
-      parameter(kmax=10)
-      logical first
-      data first/.true./
-      save first,ifac
-      if (first) then
-          ifac(1) = 1
-          ifac(2) = 1
-          do 3 j=3,kmax
-    3        ifac(j) = ifac(j-1)*(j-1)
-          first = .false.
-      end if
+          integer jderiv,l,k,d,m,   i,dd,j,mm,ndummy,ifac
+      real break(l+1),coef(d,k,l),x(m),y(d,m),   f,h
+      
+      if (jderiv .ge. k)            go to 99
 
-      do 6 mm=1,d*m
-    6    y(mm,1) = 0.
-
-      if (jderiv .ge. k) return
+      ifac = 1
+      do 4 j=2,jderiv
+    4   ifac = ifac*j
 
       do 50 mm=1,m
         call interv ( break, l+1, x(mm), i, ndummy )
         h = x(mm) - break(i)
-        f = k - jderiv
-        do 50 j=k,1+jderiv,-1
-           hf = h/f
+        do 8 dd=1,d
+    8      y(dd,mm) = coef(dd,k,i)
+        do 30 j=k-1,1+jderiv,-1
+           f = h*j/(j-jderiv)
            do 10 dd=1,d
-   10         y(dd,mm) = y(dd,mm)*hf + coef(dd,j,i)*ifac(j)
-           f = f - 1.
+   10         y(dd,mm) = y(dd,mm)*f + coef(dd,j,i)
+   30   continue
+        if (ifac .eq. 1)            go to 50
+        do 40 dd=1,d
+   40      y(dd,mm) = y(dd,mm)*ifac 
    50 continue
+      return
+   99 do 199 mm=1,d*m
+  199   y(mm,1) = 0.
       end
 
 
