adaaec4badb084508934f5cf61fcbaf39d6561c2
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2013-10-30 00:41:06 +0000

    slatec: Make first Python wrapper; deactivate bad routines
    git-svn-id: file:///svn_repos/mexfunctions.new@363 ad4b1c03-8ef8-444d-a533-6d794d5f2280

---
+++ slatec/python/setup.py
@@ -0,0 +1,28 @@
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('', parent_package, top_path)
+
+    """
+    regenerate python wrappers with:
+    > f2py -m slatec -h slatec.pyf `cat f2py_files.txt`
+    > mv pppack.pyf python
+    > cd python
+
+    then build with: sources=['slatec.pyf'], f2py_options=['--no-wrap-functions']
+
+    to switch to pregenerated slatecmodule.c:
+    > cp build/src.macosx-10.7-intel-2.7/slatecmodule.c .
+
+    and build again with no f2py_options and sources=['slatecmodule.c', 'fortranobject.c']
+    """
+    config.add_extension('slatec', 
+                         sources = ['slatec.pyf'],
+                         f2py_options = ['--no-wrap-functions'],
+                         #sources = ['slatecmodule.c', 'fortranobject.c'],
+                         libraries = ['slatec'])
+    return config
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
+
