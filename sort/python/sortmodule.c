7c73c153fa8691a3946ea515b0eb4b8b8474539e
smueller <smueller@ad4b1c03-8ef8-444d-a533-6d794d5f2280>
2014-02-04 22:01:29 +0000

    Add Swenson's sorting implementations
    - From: https://github.com/swenson/sort.git
    - Easy to use, but disappointing performance.
    
    
    git-svn-id: file:///svn_repos/mexfunctions.new@443 ad4b1c03-8ef8-444d-a533-6d794d5f2280

--- sort/python/sortmodule.c
+++ sort/python/sortmodule.c
@@ -58,12 +58,42 @@ static PyObject* stdsort_meth(PyObject *self, PyArrayObject *arr)
     Py_RETURN_NONE;
 }
 
+#define SORT_NAME swenson
+#define SORT_TYPE int
+
+#include "swenson_sort.h"
+
+static PyObject* sw_timsort_meth(PyObject *self, PyArrayObject *arr)
+{
+    int ndims = PyArray_NDIM(arr);
+    npy_intp N = PyArray_DIM(arr, ndims-1);
+    int typenum = PyArray_TYPE(arr);
+    void *x = PyArray_DATA(arr);
+
+    swenson_tim_sort(x, N);
+
+    Py_RETURN_NONE;
+}
+
+static PyObject* sw_quicksort_meth(PyObject *self, PyArrayObject *arr)
+{
+    int ndims = PyArray_NDIM(arr);
+    npy_intp N = PyArray_DIM(arr, ndims-1);
+    int typenum = PyArray_TYPE(arr);
+    void *x = PyArray_DATA(arr);
+
+    swenson_quick_sort(x, N);
+
+    Py_RETURN_NONE;
+}
 
 static PyMethodDef methods[] = {
     {"qsort", (PyCFunction)qsort_meth, METH_O, "qsort"},
     {"sort", (PyCFunction)sort, METH_O, "Sort"},
     {"quicksort", (PyCFunction)quicksort_meth, METH_O, "Quicksort"},
     {"stdsort", (PyCFunction)stdsort_meth, METH_O, "std::sort"},
+    {"sw_timsort", (PyCFunction)sw_timsort_meth, METH_O, "Swenson timsort"},
+    {"sw_quicksort", (PyCFunction)sw_quicksort_meth, METH_O, "Swenson quicksort"},
     {NULL, NULL, 0, NULL}
 };
  
